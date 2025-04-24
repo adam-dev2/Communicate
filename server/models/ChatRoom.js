151e05cfaafbb53b3cb0b930d25f6ef1bb16f0e3
adam-dev2 <shaikadam252@gmail.com>
2025-04-24 14:09:52 +0530

    working on chat application

--- server/models/ChatRoom.js
+++ server/models/ChatRoom.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const chatRoomSchema = new mongoose.Schema({
   isGroupChat: { type: Boolean, default: false },
   participants: [
-    { type: mongoose.Schema.Types.ObjectId, ref: 'User' } // reference to User model
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
   ],
-  chatName: { type: String }, // optional, mainly for group chats
+  chatName: { type: String }, 
 }, { timestamps: true });
 
 module.exports = mongoose.model('ChatRoom', chatRoomSchema);
